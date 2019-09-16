898ed19a8b05ea0894f24fbda0fbd149e5718a45
Tomas Vaskevicius <vaskevicius.tomas@gmail.com>
2019-09-16 15:17:12 +0200

    Export TensorDataset.

--- rprml/data/__init__.py
+++ rprml/data/__init__.py
@@ -1,7 +1,9 @@
 from .dataset_transforms import TransformedTensorDataset, \
     register_metric_tracking_on_transformed_dataset
+from .data_utils import TensorDataset
 
 __all__ = [
+    'TensorDataset',
     'TransformedTensorDataset',
     'register_metric_tracking_on_transformed_dataset',
 ]
